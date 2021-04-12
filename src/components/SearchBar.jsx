9236bdf9920ea735baaabc01ed31b0a324446abf
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-11 17:16:38 -0700

    event handlers with react

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,15 +2,26 @@ import React, { Component } from "react";
 
 export default class SearchBar extends Component {
   state = { input: "" };
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+  //   onInputClick() {
+  //     console.log("CLICKED");
+  //   }
   render() {
     return (
       <div className="ui segment">
         <form className="ui form" action="">
           <div className="field">
             <label htmlFor="search">Image Search</label>
-            <input type="search" id="search"></input>
+            <input
+              type="search"
+              id="search"
+              onChange={this.onInputChange}
+              //   onClick={this.onInputClick}
+            ></input>
           </div>
-          <button class="ui inverted primary button">Primary</button>
+          <button className="ui inverted primary button">Primary</button>
         </form>
       </div>
     );
