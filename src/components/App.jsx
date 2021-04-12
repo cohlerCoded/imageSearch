b7c6cf04fffa49e0d9c894a72bb75f2e8a01e57e
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-12 15:16:33 -0700

    get imgs with axios. unsplash key hidden with env

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+
+const unsplashKey = process.env.REACT_APP_UNSPLASH_KEY;
 
 export default class App extends Component {
   onSearchSubmit(input) {
-    console.log(input);
+    console.log(unsplashKey);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: input },
+      headers: { Authorization: `Client-ID ${unsplashKey}` },
+    });
   }
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 ReactDOM.render(<App />, document.querySelector("#root"));
