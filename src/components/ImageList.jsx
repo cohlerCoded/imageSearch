947b9ea9675c4f0817e5b1fac2412a2be1461870
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-13 13:00:07 -0700

    implemented grid

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,8 +1,9 @@
+import "./ImageList.css";
 import React from "react";
 
 export default function ImageList(props) {
   const images = props.images.map(({ description, id, urls }) => (
     <img key={id} src={urls.regular} alt={description} />
   ));
-  return <div className="ui segment">{images}</div>;
+  return <div className="ui segment image-list">{images}</div>;
 }
