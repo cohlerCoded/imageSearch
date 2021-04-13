026c33b83b05b947f000fbc2c5645cd77fb0e2ab
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-12 18:02:09 -0700

    refactored api req to external file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,17 @@ import SearchBar from "./SearchBar";
 const unsplashKey = process.env.REACT_APP_UNSPLASH_KEY;
 
 export default class App extends Component {
-  state = { images: [] };
-
-  onSearchSubmit = async (input) => {
+  async onSearchSubmit(input) {
     const res = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: input },
       headers: { Authorization: `Client-ID ${unsplashKey}` },
     });
-    this.setState({ images: res.data.results });
-  };
-
+    console.log(res.data.results);
+  }
   render() {
     return (
       <div className="ui container hidden divider">
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
       </div>
     );
   }
