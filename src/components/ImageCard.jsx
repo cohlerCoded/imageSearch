edf8fb83f104bc5da7059aa3fe0bae87b6aa4b71
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-13 13:58:20 -0700

    dynamic spans

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -4,19 +4,22 @@ export default class ImageCard extends Component {
   constructor(props) {
     super(props);
     this.imageRef = React.createRef();
+    this.state = { spans: null };
   }
 
   componentDidMount() {
     this.imageRef.current.addEventListener("load", this.setSpans);
   }
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
   };
 
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
